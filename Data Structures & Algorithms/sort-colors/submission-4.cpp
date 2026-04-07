5a01f4ca62447a1dad4ec433c9b248d788188df0
mln-grg <54445436+mln-grg@users.noreply.github.com>
2026-04-07 10:34:41 +0530

    Add: sort-colors - submission-4

---
+++ Data Structures & Algorithms/sort-colors/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) 
+    {
+        int left =0,right =nums.size()-1,i =0;
+
+        while(i<=right)
+        {
+            if(nums[i] == 0)
+            {
+                swap(nums[i],nums[left]);
+                left++;
+            }
+            else if(nums[i] ==2)
+            {
+                swap(nums[i],nums[right]);
+                right--;
+                i--;
+            }
+            
+            i++;
+        }
+    }
+};
